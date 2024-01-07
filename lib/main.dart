dda8a4bfaf00a341265efc40e2946b52c35b64af
kathik1772 <kathikamma2004@gmail.com>
2024-01-07 23:41:02 +0530

    pending

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       onGenerateRoute: Routes.onGenerate,
-      initialRoute: '/login',
-    );
+      initialRoute: '/login',);
   }
 }
